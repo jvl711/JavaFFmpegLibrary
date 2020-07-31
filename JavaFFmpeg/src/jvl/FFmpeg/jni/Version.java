12359ae6864d4ba71913cce78937e1d687826823
Joshua V. Lewis <josh@joshandmonique.com>
2020-07-31 13:45:43 -0400

    Fixing auto build

--- JavaFFmpeg/src/jvl/FFmpeg/jni/Version.java
+++ JavaFFmpeg/src/jvl/FFmpeg/jni/Version.java
@@ -3,8 +3,8 @@ package jvl.FFmpeg.jni;
 
 public class Version
 {
-    private final static String BUILDTIME = "07/31/2020 13:14:54";
-    private final static String BUILDNUMBER = "469";
+    private final static String BUILDTIME = "07/31/2020 13:45:13";
+    private final static String BUILDNUMBER = "483";
     private final static String VERSION = "0.4-alpha";
     
     public static String getVersion()
